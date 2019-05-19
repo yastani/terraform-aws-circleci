df7fda8a8379ee58e18af42aa1d1e7e0ab2f48d5
yastani <tech@nati.jp>
2019-05-20 00:42:21 +0900

    terraform cloudはworkspaceの管理がしづらいためS3に戻す

--- backend.tf
+++ backend.tf
@@ -2,16 +2,11 @@
 # Terraform Settings
 #####################################
 terraform {
-  # Terraform Cloudを使用するためバージョンは最低でも0.11.13以上
   # チーム開発の場合は基本的に固定しないとtfstateの管理バージョンで怒られる
   required_version = ">= 0.11.13"
 
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "yastani"
-
-    workspaces {
-      prefix = "yastani-"
-    }
+  backend "s3" {
+    bucket = "yastani-terraform-tfstates"
+    region = "ap-northeast-1"
   }
 }
